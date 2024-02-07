e12c34b92f2a672905e78df96867335d33828179
Yuan_Ming <yuanming.22@ichat.sp.edu.sg>
2024-02-08 05:53:40 +0800

    re-added the detailsactivity.

--- app/src/main/java/com/sp/mobileappliancesdirector/DetailsActivity.java
+++ app/src/main/java/com/sp/mobileappliancesdirector/DetailsActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -16,17 +22,57 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        // Retrieve data from the intent or other sources to display details
+        // Retrieve data from the intent to display details
         Intent intent = getIntent();
         String applianceType = intent.getStringExtra("APPLIANCE_TYPE");
         String applianceModel = intent.getStringExtra("APPLIANCE_MODEL");
-        String applianceImageUrl = intent.getStringExtra("APPLIANCE_IMAGE_URL");
-        Log.d("DetailsActivity", "Appliance Type: " + applianceType);
-        Log.d("DetailsActivity", "Appliance Model: " + applianceModel);
-        Log.d("DetailsActivity", "Appliance Image URL: " + applianceImageUrl);
+        String applianceImageUrl = intent.getStringExtra("APPLIANCES_IMAGE_URL");
 
         setTitle(applianceType + " Details");
+
         ImageView imageView = findViewById(R.id.detailsImageView);
+        TextView applianceTypeTextView = findViewById(R.id.detailsApplianceType);
+        TextView applianceModelTextView = findViewById(R.id.detailsApplianceModel);
+        Button buttonOn = findViewById(R.id.buttonOn);
+        Button buttonOff = findViewById(R.id.buttonOff);
+        Spinner dropdownTimer = findViewById(R.id.dropdownTimer);
+        Button buttonSetTimer = findViewById(R.id.buttonSetTimer);
+
         Picasso.with(this).load(applianceImageUrl).into(imageView);
+
+        // Set the appliance type and appliance model in the TextViews
+        applianceTypeTextView.setText(applianceType);
+        applianceModelTextView.setText(applianceModel);
+
+        // Set click listener for the On button
+        buttonOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailsActivity.this, "Device is switched on", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set click listener for the Off button
+        buttonOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailsActivity.this, "Device is switched off", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set up the spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.timer_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdownTimer.setAdapter(adapter);
+
+        // Set click listener for the Set Timer button
+        buttonSetTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectedTime = dropdownTimer.getSelectedItem().toString();
+                Toast.makeText(DetailsActivity.this, "Device is turned on for " + selectedTime, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
